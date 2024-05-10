9dd6d40370c6172c8196077d0dc1fb20f5913661
Renaud Hartert <91352809+rhartert@users.noreply.github.com>
2024-05-10 16:27:03 +0900

    Simplify function allocSlice

--- internal/sat/clauses_alloc.go
+++ internal/sat/clauses_alloc.go
@@ -12,28 +12,17 @@ const nPools = 4
 const lastCapa = 1 << nPools
 
 // Pools of slices with different capacities so that pool i contains slices with
-// a capacity between 2^(i+1) and 2^(i+2)-1 inclusive. The last pool k contains
-// slices with a capacity of at least 2^(k+1).
-//
-// The pools are initiated by the init function below.
+// a capacity between 2^(i+1) and 2^(i+2)-1 inclusive. The last pool k has no
+// upper bound and contains slices with a capacity of at least 2^(k+1).
 var pools = [nPools]sync.Pool{}
 
-func init() {
-	for i := 0; i < nPools; i++ {
-		capa := 1 << (i + 1)
-		pools[i].New = func() any {
-			s := make([]Literal, 0, capa)
-			return &s
-		}
-	}
-}
-
-// pid returns the ID of the pool responsible for slice of the given capacity.
+// pid returns the ID of the smallest pool that can serve a slice of the
+// requested capacity.
 func pid(capa int) int {
-	if capa >= lastCapa {
+	if lastCapa <= capa {
 		return nPools - 1
 	}
-	pid := bits.Len(uint(capa)) - 1
+	pid := max(bits.Len(uint(capa))-1, 0)
 	if capa < (1 << pid) {
 		pid--
 	}
@@ -42,20 +31,25 @@ func pid(capa int) int {
 
 // allocLiteral returns an empty slice that has at least the requested capacity.
 func allocSlice(capa int) *[]Literal {
-	ref := pools[pid(capa)].Get().(*[]Literal)
-	if capa < lastCapa {
-		return ref
+	pid := pid(capa)
+
+	ref := pools[pid].Get()
+	if ref != nil && capa <= cap(*ref.(*[]Literal)) {
+		return ref.(*[]Literal)
 	}
 
-	// If the slice comes from the last pool, we need to ensure that it has
-	// enough capacity. If not, we drop the returned slice and replace it with
-	// a newly allocated slice that has the requested capacity.
-	if cap(*ref) < capa {
-		s := make([]Literal, 0, capa)
-		ref = &s
+	if pid < nPools-1 {
+		s := make([]Literal, 0, 2<<pid)
+		return &s
 	}
 
-	return ref
+	if capa <= lastCapa*2 {
+		s := make([]Literal, 0, lastCapa*2)
+		return &s
+	}
+
+	s := make([]Literal, 0, capa)
+	return &s
 }
 
 // freeSlice returns the reference slice so that it can be allocated to another
